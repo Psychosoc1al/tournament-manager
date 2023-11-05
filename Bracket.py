ab6c29373218bcf65d5c2f75cb103b132a75b3f0
ZzVaMPIRzZ <mitroshin.dmitry2003@gmail.com>
2023-11-06 00:12:20 +0300

    добавил создание нижней сетки TODO: доделать обновление результатов, продумать перекидывание лузеров в нижнюю сетку

--- Bracket.py
+++ Bracket.py
@@ -37,37 +37,57 @@ class Bracket:
 
     def generate_bracket(self, participants: list[Participant]):
         start_size = len(participants)
-        k = 2
-        while k < start_size:
-            k *= 2
-        size = k
-        self.matches.append([])
-        for i in range(start_size - size//2):
-            self.matches[0].append(Match(0, i, participants[2*i], participants[2*i+1]))
-        for i in range((start_size - size//2)*2, start_size):
-            self.matches[0].append(Match(0, i, participants[i], Participant(None)))
-        size //= 2
-        while size > 1:
+        size = 2
+        while size < start_size:
+            size *= 2
+        if self.type == BracketType.UPPER or self.type == BracketType.SINGLE:
             self.matches.append([])
-            for i in range(size//2):
-                self.matches[-1].append(Match(len(self._matches)-1, i, Participant(None), Participant(None)))
-                if self.matches[-2][2*i].participant2.name is None and\
-                        self.matches[-2][2*i].participant1.name is not None:
-                    self.matches[-1][-1].participant1 = self.matches[-2][2*i].participant1
-                if self.matches[-2][2*i + 1].participant2.name is None and\
-                        self.matches[-2][2*i + 1].participant1.name is not None:
-                    self.matches[-1][-1].participant2 = self.matches[-2][2*i + 1].participant1
+            for i in range(start_size - size//2):
+                self.matches[0].append(Match(0, i, participants[2*i], participants[2*i+1]))
+            for i in range((start_size - size//2)*2, start_size):
+                self.matches[0].append(Match(0, i, participants[i], Participant(None)))
             size //= 2
+            while size > 1:
+                self.matches.append([])
+                for i in range(size//2):
+                    self.matches[-1].append(Match(len(self._matches)-1, i, Participant(None), Participant(None)))
+                    if self.matches[-2][2*i].participant2.name is None and\
+                            self.matches[-2][2*i].participant1.name is not None:
+                        self.matches[-1][-1].participant1 = self.matches[-2][2*i].participant1
+                    if self.matches[-2][2*i + 1].participant2.name is None and\
+                            self.matches[-2][2*i + 1].participant1.name is not None:
+                        self.matches[-1][-1].participant2 = self.matches[-2][2*i + 1].participant1
+                size //= 2
+        elif self.type == BracketType.LOWER:
+            size //= 4
+            while size >= 1:
+                for _ in range(2):
+                    self.matches.append([])
+                    for i in range(size):
+                        self.matches[-1].append(Match(len(self._matches)-1, i, Participant(None), Participant(None)))
+                size //= 2
+        else:
+            raise ValueError
 
     def update_result(self, stage: int, match_number_stage: int, result: (int, int)):
         self.matches[stage][match_number_stage].score_participant1 = result[0]
         self.matches[stage][match_number_stage].score_participant2 = result[1]
         if result[0] > result[1]:
             winner = self.matches[stage][match_number_stage].participant1
+            loser = self.matches[stage][match_number_stage].participant2
         else:
             winner = self.matches[stage][match_number_stage].participant2
-        if match_number_stage % 2 == 0:
-            self.matches[stage + 1][match_number_stage//2].participant1 = winner
-        else:
-            self.matches[stage + 1][match_number_stage//2].participant2 = winner
+            loser = self.matches[stage][match_number_stage].participant1
+
+        if self.type == BracketType.UPPER or self.type == BracketType.SINGLE:   #TODO
+            if match_number_stage % 2 == 0:
+                self.matches[stage + 1][match_number_stage//2].participant1 = winner
+            else:
+                self.matches[stage + 1][match_number_stage//2].participant2 = winner
+        elif self.type == BracketType.LOWER:
+            pass
+            # if match_number_stage % 4 == 0:
+            #     self.matches[stage + 1][match_number_stage//4].participant1 = winner
+            # else:
+            #     self.matches[stage + 1][match_number_stage//4].participant2 = winner
 
