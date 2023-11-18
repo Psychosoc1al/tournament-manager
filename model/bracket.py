8091820e3da0544d230f026fb3bbc57c5e4d9ca3
Psychosoc1al <63284142+Psychosoc1al@users.noreply.github.com>
2023-11-18 23:12:35 +0300

    Fix branches merging bug

--- model/bracket.py
+++ model/bracket.py
@@ -33,27 +33,6 @@ class Bracket:
 
     def generate_bracket(self, participants: list[Participant]) -> None:
         start_size = len(participants)
-        k = 2
-        while k < start_size:
-            k *= 2
-        size = k
-        self.matches.append([])
-        for i in range(start_size - size // 2):
-            self.matches[0].append(Match(0, i, participants[2 * i], participants[2 * i + 1]))
-        for i in range((start_size - size // 2) * 2, start_size):
-            self.matches[0].append(Match(0, i, participants[i], Participant()))
-        size //= 2
-        while size > 1:
-            self.matches.append([])
-            for i in range(size // 2):
-                self.matches[-1].append(Match(len(self.matches) - 1, i, Participant(), Participant()))
-                if self.matches[-2][2 * i].participant2.name is None and \
-                        self.matches[-2][2 * i].participant1.name is not None:
-                    self.matches[-1][-1].participant1 = self.matches[-2][2 * i].participant1
-                if self.matches[-2][2 * i + 1].participant2.name is None and \
-                        self.matches[-2][2 * i + 1].participant1.name is not None:
-                    self.matches[-1][-1].participant2 = self.matches[-2][2 * i + 1].participant1
-            size //= 2
         size = 2
         while size < start_size:
             size *= 2
