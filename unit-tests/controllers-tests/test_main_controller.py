fa53d65d41a9eb3f6c779025e5ba18ee408b91c5
Psychosoc1al <63284142+Psychosoc1al@users.noreply.github.com>
2023-12-05 03:02:59 +0300

    Finish main_controller testing

--- unit-tests/controllers-tests/test_main_controller.py
+++ unit-tests/controllers-tests/test_main_controller.py
@@ -1,4 +1,5 @@
-from unittest.mock import Mock, patch
+from datetime import date
+from unittest.mock import Mock, patch, MagicMock
 
 import pytest
 
@@ -21,11 +22,42 @@ class TestMainController:
             1 / 2, 1 / 2
         )
 
-    # noinspection PyUnusedLocal
+    @patch("controllers.main_controller.Tournament")
+    @patch("controllers.main_controller.Participant")
+    @patch("controllers.main_controller.AddEditPageController")
+    @patch("controllers.main_controller.AddEditPageView")
+    def test_add_button_click(
+        self,
+        _,
+        AddEditPageController: MagicMock,
+        Participant: MagicMock,
+        Tournament: MagicMock,
+        main_controller: MainController,
+    ):
+        Tournament.return_value = MagicMock()
+
+        main_controller.view.add_tournament_button.clicked.connect.assert_called_once()
+
+        main_controller._add_tournament_show()
+
+        AddEditPageController.return_value.form_submitted.connect.assert_called_once()
+
+        main_controller._add_tournament(
+            "Name",
+            "Sport",
+            "Type",
+            date(2020, 1, 1),
+            "Participants",
+        )
+
+        Tournament.assert_called_once_with(
+            "Name", "Sport", "Type", date(2020, 1, 1), [Participant("Participants")]
+        )
+
     @patch("controllers.main_controller.AddEditPageController")
     @patch("controllers.main_controller.AddEditPageView")
     def test_update_button_click(
-        self, AddEditPageView, AddEditPageController, main_controller: MainController
+        self, _, AddEditPageController: MagicMock, main_controller: MainController
     ):
         update_button = Mock()
         tournament = Mock()
@@ -36,9 +68,59 @@ class TestMainController:
         main_controller.view.show_tournaments.return_value = {"update": [update_button]}
         main_controller.show_main_page()
 
-        assert main_controller._updating_tournament == tournament
+        AddEditPageController.return_value.form_submitted.connect.assert_called_once()
+        main_controller._update_tournament(
+            "Name",
+            "Sport",
+            "",
+            date(2020, 1, 1),
+            "",
+        )
 
-    @pytest.fixture(autouse=True)
+    def test_remove_button_click(self, main_controller: MainController):
+        remove_button = Mock()
+        tournament = Mock()
+        remove_button.clicked.connect.side_effect = (
+            lambda _: main_controller._remove_tournament(tournament)
+        )
+
+        main_controller.view.show_tournaments.side_effect = [
+            {"remove": [remove_button]},
+            {},
+        ]
+        main_controller.show_main_page()
+
+        main_controller.model.delete_tournament.assert_called_once_with(tournament)
+
+    @patch("controllers.main_controller.TournamentPageController")
+    @patch("controllers.main_controller.TournamentPageView")
+    def test_tournament_button_click(
+        self, TournamentPageView: MagicMock, __, main_controller: MainController
+    ):
+        tournament_button = Mock()
+        tournament = MagicMock()
+        tournament.participants.__len__.return_value = 4
+        tournament_button.clicked.connect.side_effect = (
+            lambda _: main_controller._go_to_tournament(tournament)
+        )
+        tournament_view = TournamentPageView.return_value
+
+        main_controller.view.show_tournaments.return_value = {
+            "tournaments": [tournament_button]
+        }
+        main_controller.show_main_page()
+
+        main_controller.view.resize_screen_percent_and_center.assert_any_call(
+            3 / 4, 5 / 6
+        )
+        main_controller.view.central_stacked_widget.addWidget.assert_called_once_with(
+            tournament_view
+        )
+        main_controller.view.central_stacked_widget.setCurrentWidget.assert_called_once_with(
+            tournament_view
+        )
+
+    @pytest.fixture()
     def main_controller(self):
         main_page = Mock()
         main_page_view = Mock()
