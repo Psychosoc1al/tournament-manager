7e4a6d58590b63809d8b0d637d26e79371ab49f5
ZzVaMPIRzZ <mitroshin.dmitry2003@gmail.com>
2023-12-21 23:14:48 +0300

    1

--- features/steps/mainPageDef.py
+++ features/steps/mainPageDef.py
@@ -1,16 +1,16 @@
-from behave import *  # pip install behave
+from behave import given, when, then  # pip install behave
 
 
-@given(u'I open programm')
+@given("I open programm")
 def step_impl(context):
     context.a = 4
 
 
-@when(u'I press add button')
+@when("I press add button")
 def step_impl(context):
     context.a += 1
 
 
-@then(u'Add window opens')
+@then("Add window opens")
 def step_impl(context):
     assert context.a == 5
